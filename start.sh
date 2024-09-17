01ceb161666a554fe93bb782f0917865aeb8feeb
Samia CHAYEB <samia.chayeb@sofrecom.com>
2024-09-18 00:17:45 +0200

    feat: add flv file to volume

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run ffmpeg command
-ffmpeg -stream_loop -1 -re -i flv/file.flv -fflags +genpts -r 30 -c copy -f flv rtmp://rtmp-80oe.onrender.com:1936/live/stream
+ffmpeg -stream_loop -1 -re -i /usr/local/flv/file.flv -fflags +genpts -r 30 -c copy -f flv rtmp://rtmp-80oe.onrender.com:1936/live/stream
 
 # Start NGINX
 nginx -g 'daemon off;'
